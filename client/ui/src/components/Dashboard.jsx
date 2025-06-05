d309932aa9de4f57c470cf58afbd8e26e1f059aa
manmay2 <imanmay2@gmail.com>
2025-06-05 19:00:00 +0530

    50% Dashboad UI completed

--- client/ui/src/components/Dashboard.jsx
+++ client/ui/src/components/Dashboard.jsx
@@ -1,11 +1,63 @@
 import "./css/dashboard.css";
-import {Options} from "./Options"
-function Dashboard(){
-    return(
+import { Options } from "./Options";
+import { User } from 'lucide-react';
+import { HiOutlineFaceSmile } from "react-icons/hi2";
+import { RiArrowDownSLine } from "react-icons/ri";
+function Dashboard() {
+    let name = "Manmay";
+    let username = "imanmay2";
+
+    //fetch date:
+    function getFormattedDate() {
+        const date = new Date();
+
+        const days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+        const months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun",
+            "Jul", "Aug", "Sept", "Oct", "Nov", "Dec"];
+
+        const dayName = days[date.getDay()];
+        const day = date.getDate();
+        const monthName = months[date.getMonth()];
+        const year = date.getFullYear();
+
+        return `${dayName}, ${day} ${monthName} ${year}`;
+    }
+
+    return (
         <div className="dashboard">
-            <Options/>
+            <Options />
+            <div className="main">
+                <div className="nav">
+                    <font className="designDash">Dashboard</font>
+
+                    <div id="details">
+                        <User size={38} strokeWidth={2} /> &nbsp; &nbsp;
+                        <div className="name">
+                            <b>{name}</b> <br />
+                            <font id="username">{username}</font>
+                        </div> <RiArrowDownSLine />
+                    </div>
+                </div>
+
+                <br />
+                <center> <hr className="line" width="95%" /></center>
+
+                <br />
+                <div className="greet">
+                    <div className="content">
+                        <h2> Welcome Back , Mr. {name} !  <HiOutlineFaceSmile /> </h2>
+                        <div className="space">
+                            <font className="quote">Always take care of your health to carry out Smart.</font></div>
+                    </div>
+                    <div id="date">
+                       {getFormattedDate()}
+                    </div>
+                </div>
+
+
+            </div>
         </div>
     )
 }
 
-export {Dashboard};
+export { Dashboard };
