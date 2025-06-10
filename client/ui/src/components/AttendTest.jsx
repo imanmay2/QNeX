b0829e297c1bd32aaab0f393088631988a0b2da6
Manmay Chakraborty <imanmay2@gmail.com>
2025-06-10 21:18:19 +0530

    createTest doing tommo

--- client/ui/src/components/AttendTest.jsx
+++ client/ui/src/components/AttendTest.jsx
@@ -1,12 +1,16 @@
 import "./css/attendTest.css";
 import { Options } from "./Options";
-
 function AttendTest(){
     return(
         <div className="attendTest">
             <Options/>
+            
             <div className="main_">
+                <font className="heading">Attend Tests</font>
+                <div className="askid">
+                    <center>Test ID</center>
 
+                </div>
             </div>
         </div>
     );
