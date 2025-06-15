9a340ea1e363ddf3a5c2b2f87add5f8d7f7c03f3
manmay2 <imanmay2@gmail.com>
2025-06-15 20:11:13 +0530

    input handle done

--- client/ui/src/components/CreateTest.jsx
+++ client/ui/src/components/CreateTest.jsx
@@ -19,10 +19,16 @@ function CreateTest() {
         }]
     });
     let handleInput = ((event) => {
-
+        let {name,value}=event.target;
+        setTest((currData)=>{
+            if(["testTitle","description","duration","min"].includes(name)){
+                return{...currData,[name]:value};
+            }
+            return currData;
+        })
     })
 
-    let [Q, setQ] = useState([{
+    let [Q, setQ] = useState({
         question: "",
         options: {
             option_A: "",
@@ -30,7 +36,23 @@ function CreateTest() {
             option_C: "",
         },
         ans: ""
-    }]);
+    });
+
+    let handleQ=((event)=>{
+        let {name,value}=event.target;
+        setQ((currData)=>{
+            if(["question","ans"].includes(name)){
+                return {...currData,[name]:value}
+            } else if(["option_A","option_B","option_C"].includes(name)){
+                return {
+                    ...currData, options:{
+                        ...currData.options,[name]:value
+                    }
+                }
+            }
+            return currData;
+        })
+    })
     
 
     return (
@@ -65,26 +87,28 @@ function CreateTest() {
                         </div>
                         <br /><br /><br />
                         <h2>Add Questions</h2>
+
+
                         <div className="addQ">
                             <label htmlFor="Question" id="question_1_label">Question 1</label><br />
-                            <input type="text" id="Question" name="question" value={test.questions_.question} onChange={handleInput} style={{ width: "90vh" }} /> <br /><br /><br />
+                            <input type="text" id="Question" name="question" value={Q.question} onChange={handleQ} style={{ width: "90vh" }} /> <br /><br /><br />
                             <div className="options">
                                 <span>
                                     <label htmlFor="option_A">Option A</label><br />
-                                    <input type="text" id="option_A" name="option_A" value={test.questions_.options.option_A} onChange={handleInput} style={{ width: "30vh" }} />
+                                    <input type="text" id="option_A" name="option_A" value={Q.options.option_A} onChange={handleQ} style={{ width: "30vh" }} />
                                 </span>
                                 <span>
                                     <label htmlFor="option_B">Option B</label><br />
-                                    <input type="text" id="option_B" name="option_B" value={test.questions_.options.option_B} onChange={handleInput} style={{ width: "30vh" }} />
+                                    <input type="text" id="option_B" name="option_B" value={Q.options.option_B} onChange={handleQ} style={{ width: "30vh" }} />
                                 </span>
                                 <span>
                                     <label htmlFor="option_C">Option C</label><br />
-                                    <input type="text" id="option_C" name="option_C" value={test.questions_.options.option_C} onChange={handleInput} style={{ width: "30vh" }} />
+                                    <input type="text" id="option_C" name="option_C" value={Q.options.option_C} onChange={handleQ} style={{ width: "30vh" }} />
                                 </span>
                             </div>
                             <br /><br />
                             <label htmlFor="correctAns">Correct Answer</label><br />
-                            <input type="text" id="correctAns" name="ans" value={test.questions_.ans} onChange={handleInput} style={{ width: "30vh" }} />
+                            <input type="text" id="correctAns" name="ans" value={Q.ans} onChange={handleQ} style={{ width: "30vh" }} />
                             <span className="add_anotherQ">+ Add Another Question</span>
                             <span className="delete">Delete Question</span>
                         </div>
